ef0b9702d63a85b44bacb57f0322497da20718ba
Nicolas Crausaz <n.crausaz99@gmail.com>
2022-11-29 14:29:10 +0100

    Init project

---
+++ src/test/java/ch/heig/amtteam10/microservices/MicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.heig.amtteam10.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
