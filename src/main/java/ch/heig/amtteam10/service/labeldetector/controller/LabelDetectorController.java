52408e587e98928c296d19d9e5835d3162441830
Nicolas Crausaz <n.crausaz99@gmail.com>
2022-11-29 15:11:02 +0100

    Build a .jar for each microservice

--- src/main/java/ch/heig/amtteam10/service/labeldetector/controller/LabelDetectorController.java
+++ src/main/java/ch/heig/amtteam10/service/labeldetector/controller/LabelDetectorController.java
@@ -4,9 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class DataObjectController {
+public class LabelDetectorController {
     @GetMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "This is the label detector!";
     }
 }
