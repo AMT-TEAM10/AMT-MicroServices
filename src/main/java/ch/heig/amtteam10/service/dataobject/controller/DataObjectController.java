7b735ea642d7042d8098ffc69a95e33ec5d53137
Nicolas Crausaz <n.crausaz99@gmail.com>
2022-11-29 15:49:23 +0100

    Get file endpoint ok

--- src/main/java/ch/heig/amtteam10/service/dataobject/controller/DataObjectController.java
+++ src/main/java/ch/heig/amtteam10/service/dataobject/controller/DataObjectController.java
@@ -1,12 +1,35 @@
 package ch.heig.amtteam10.service.dataobject.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import ch.heig.amtteam10.core.cloud.AWSClient;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class DataObjectController {
-    @GetMapping("/")
-    public String index() {
-        return "This is the DataObject controller !";
+    @GetMapping("/object/{objectName}")
+    public ResponseEntity<Resource> index(@PathVariable String objectName) {
+        if (objectName.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        ByteArrayResource resource = new ByteArrayResource(AWSClient.getInstance().dataObject().get(objectName));
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .contentLength(resource.contentLength())
+                .header(HttpHeaders.CONTENT_DISPOSITION,
+                        ContentDisposition.attachment()
+                                .filename(objectName)
+                                .build().toString())
+                .body(resource);
+    }
+
+    @PostMapping("/object/{objectName}")
+    public ResponseEntity<Resource> create(@PathVariable String objectName) {
+
+        // AWSClient.getInstance().dataObject().create(objectName, );
+
     }
 }
