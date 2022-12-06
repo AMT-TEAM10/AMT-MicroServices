5ea6acf5d39ec5525a34258c4da5a615f91fe7dc
Maxime Scharwath <maxscharwath@gmail.com>
2022-12-06 16:09:03 +0100

    test

--- labeldetector/src/main/java/ch/heig/amtteam10/labeldetector/controller/LabelDetectorController.java
+++ labeldetector/src/main/java/ch/heig/amtteam10/labeldetector/controller/LabelDetectorController.java
@@ -4,6 +4,7 @@ import ch.heig.amtteam10.labeldetector.core.AWSClient;
 import ch.heig.amtteam10.labeldetector.core.Label;
 import ch.heig.amtteam10.labeldetector.core.exceptions.FailDownloadFileException;
 import ch.heig.amtteam10.labeldetector.dao.ProcessDAO;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +16,7 @@ import org.springframework.web.server.ResponseStatusException;
 @RestController
 public class LabelDetectorController {
     @PostMapping("/process")
-    public @ResponseBody ResponseEntity<Label[]> processImage(@RequestBody ProcessDAO tmp) {
+    public @ResponseBody ResponseEntity<Label[]> processImage(@Valid @RequestBody ProcessDAO tmp) {
         try {
             var labels = AWSClient.getInstance().labelDetector().execute(tmp.imageUrl(), tmp.maxLabels(), tmp.minConfidence());
             return ResponseEntity.ok(labels);
--- labeldetector/src/main/java/ch/heig/amtteam10/labeldetector/dao/ProcessDAO.java
+++ labeldetector/src/main/java/ch/heig/amtteam10/labeldetector/dao/ProcessDAO.java
@@ -1,4 +1,16 @@
 package ch.heig.amtteam10.labeldetector.dao;
 
-public record ProcessDAO(String imageUrl, int maxLabels, float minConfidence) {
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
+public record ProcessDAO(
+        @NotBlank(message = "imageUrl is mandatory")
+        String imageUrl,
+        @Min(value = 1, message = "maxLabels must be greater than 0")
+        int maxLabels,
+        @Min(value = 0, message = "minConfidence must be greater than 0")
+        @Max(value = 1, message = "minConfidence must be lower than 1")
+        float minConfidence
+) {
 }
